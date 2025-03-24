19ca53c8f5870b181b86ab365ac2380b72263765
Ivan Vorobev <ivan.vorobjov@gmail.com>
2025-03-24 21:15:06 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
+
+def load_json(filename='data/posts.json'):
+    with open(filename, 'r', encoding='utf-8') as f:
+        posts = json.load(f)
+    return posts
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/blog/')
+def blog():
+    posts = load_json()
+    return render_template('blog.html', posts=posts)
+
+@app.route('/contacts/')
+def contacts():
+    contacts = load_json(filename='data/contacts.json')
+    return render_template('contacts.html', contacts=contacts)
 
 if __name__ == '__main__':
     app.run(debug=True)
